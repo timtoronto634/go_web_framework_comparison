316fbd83fe5b8e3daead3736c44c8d58c1b9b078
timtoronto634 <tim225x@gmail.com>
2023-08-11 21:58:25 +0900

    finish quick start

--- echo/server.go
+++ echo/server.go
@@ -23,6 +23,16 @@ func main() {
 	e.GET("/show", show)
 	e.POST("/save", save)
 
+	e.POST("/users", func(c echo.Context) error {
+		u := new(User)
+		if err := c.Bind(u); err != nil {
+			return err
+		}
+		return c.JSON(http.StatusCreated, u)
+	})
+
+	e.Static("/static", "static")
+
 	e.Logger.Debug(e.Start(":1323"))
 }
 
@@ -70,4 +80,9 @@ func save(c echo.Context) error {
 	}
 
 	return c.HTML(http.StatusOK, "<b>Thank you! "+name+"</b>")
+}
+
+type User struct {
+	Name  string `json:"name" xml:"name" form:"name" query:"name"`
+	Email string `json:"email" xml:"email" form:"email" query:"email"`
 }
